804a69a8142e08e062c7e6c1672e19e0b0aae5c7
Izabela <izab7212119@gmail.com>
2025-01-28 16:28:23 +0100

    Update Operators.js

--- Operators.js
+++ Operators.js
@@ -15,3 +15,19 @@ console.log(1+"2"); //12
 console.log(true+true); //2! we convert true to 1
 console.log(2+null); //2, null = 0
 console.log(2+undefined); //NaN 
+//?? - give me first correct value - not undefined or null - ES2020
+var x = null;
+var y;
+var z = 15;
+var result = x??y??z;
+console.log(result);
+console.log(typeof null); //OBJECT!!!
+const funABC = () => {return "abc";};
+console.log(typeof funABC); //function
+let tab = [1,2,3,4,5];
+console.log(typeof tab); //OBJECT
+delete tab[3]; // if it deleted the element it return true
+for(var i = 0; i < 5; i++)
+{
+	console.log("i = "+tab[i]);
+}
