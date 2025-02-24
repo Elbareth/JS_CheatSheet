371985f3e4ffd783dbd7ef2abae5cd580a277896
Izabela <izab7212119@gmail.com>
2025-02-24 16:01:17 +0100

    Update ObjectTopic.js

--- ObjectTopic.js
+++ ObjectTopic.js
@@ -19,3 +19,14 @@ let arr = new Array();
 let obj1 = Object.create({x:5,y:10,z:15}); //obj1 inherit the argument prototype
 let obj2 = Object.create(null); //in that case - this method inherit nothing! Even toString() method!
 let obj3 = Object.create(Object.prototype); //just standard object like new Object();
+let obj4 = new Object();
+for(let i = 'a', k = 0; i < 'z'; i++, k++)
+{
+	console.log(i + " " + k);
+  obj4[i] = k; //it's dynamically calculated
+}
+obj4.abc = 144; //it's static
+for(let [key, value] of Object.entries(obj4))
+{
+	console.log(key +" = "+value);
+}
